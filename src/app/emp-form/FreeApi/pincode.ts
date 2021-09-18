c9eeee738e599da9d29f91917697613d8e40a6ce
Suchit Koli <suchitkoli97@gmail.com>
2021-09-18 18:39:57 +0530

    Add files via upload

---
+++ src/app/emp-form/FreeApi/pincode.ts
@@ -0,0 +1,17 @@
+
+
+
+export class pincode{
+    // id!:string ;
+    // country!:string;
+    pincode!:string;
+    // postalLocation!: string
+    // state!: string
+    // stateId!: string
+    // district!: string
+    // districtId!:string
+    // province!:string
+    // provinceId!:string
+    // latitude!: string
+    // longitude!: string
+}
